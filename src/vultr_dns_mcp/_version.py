9acb38870875cda8df8cf0dae3521e06edc6982b
Ryan Malloy <ryan@supported.systems>
2025-06-12 05:25:08 -0600

    dynamic version

--- src/vultr_dns_mcp/_version.py
+++ src/vultr_dns_mcp/_version.py
@@ -1,4 +1,8 @@
 """Version information for vultr-dns-mcp package."""
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version
 
-__version__ = "1.0.1"
+__version__ = version("vultr_dns_mcp")
 __version_info__ = tuple(int(i) for i in __version__.split(".") if i.isdigit())
